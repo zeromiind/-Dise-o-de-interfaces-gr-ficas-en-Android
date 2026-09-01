37419cbd1616f867d1ef90a3b5f5ef876564a987
zero <scrdyviolet@gmail.com>
2026-09-01 11:48:04 -0500

    Cambio 3

--- app/src/main/java/com/example/utilidades2/Toats.java
+++ app/src/main/java/com/example/utilidades2/Toats.java
@@ -1,6 +1,9 @@
 package com.example.utilidades2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Toats extends AppCompatActivity {
+
+    Button btn1, btn2;
+
+    private void loadUI(){
+        btn1 = findViewById(R.id.btnAvisoLento);
+        btn2 = findViewById(R.id.btnAvisoRapido);
+    }
+
+    private void notificar(String mensaje, boolean tiempo){
+        //sentencia IF en una sola linea :p
+        int duracion = (tiempo) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT;
+        Toast.makeText(getApplicationContext(),mensaje,duracion).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +36,19 @@ public class Toats extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        this.loadUI();
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notificar("Hola amigos",true);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notificar("Adios amigos",false);
+            }
         });
     }
 }
